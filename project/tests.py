6e2f93fa83db9310183bac0affa45d36ce0d87d9
lvanekl <83428939+lvanekl@users.noreply.github.com>
2023-10-09 19:29:52 +0300

    Поправил варнинги из прошлого комита.
    Дописал докстроки в класс и функцию
    Отключил проверку док стрингов у модулей

--- project/tests.py
+++ project/tests.py
@@ -3,6 +3,9 @@ from django.test import TestCase
 
 # Create your tests here.
 class MathTest(TestCase):
+    """Этот класс я создал чтобы тестировать настройку CI/CD"""
+
     def test_some_math(self):
+        """Эта функция - пустой тест. Нужна чтобы я мог тестировать CI/CD"""
         self.assertEqual(2 / 1, 2)
         self.assertEqual(0 / 2, 0)
