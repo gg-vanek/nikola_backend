061f57187fac7b9df07e6aa7ef26c7b138358892
lvanekl <83428939+lvanekl@users.noreply.github.com>
2023-10-09 18:25:33 +0300

    Added blank test

---
+++ project/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+
+# Create your tests here.
+class MathTest(TestCase):
+    def test_some_math(self):
+        self.assertEqual(2 / 1, 2)
+        self.assertEqual(0 / 1, 0)
