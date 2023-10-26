c4578e676567171ccb195b03e99a49dd7bb92b15
lvanekl <83428939+lvanekl@users.noreply.github.com>
2023-10-26 15:13:14 +0300

    Я понял, что мне не нужен эндпоинт со страницей отдельного домика - вся эта информация уже есть в списке домиков

--- houses/views.py
+++ houses/views.py
@@ -1,5 +1,4 @@
 from rest_framework import mixins
-from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from core.mixins import ByActionMixin
@@ -11,12 +10,10 @@ from houses.serializers import HouseFeatureListSerializer, HouseListSerializer
 
 class HouseViewSet(ByActionMixin,
                    mixins.ListModelMixin,
-                   mixins.RetrieveModelMixin,
                    GenericViewSet):
     serializer_classes_by_action = {
         "default": None,
         "list": HouseListSerializer,
-        "retrieve": None,
     }
     queryset = House.objects.all()
 
