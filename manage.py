618f6bfc84e89cd90808cad4c248746cad17c760
lvanekl <83428939+lvanekl@users.noreply.github.com>
2024-02-24 02:48:27 +0300

    removed comments from manage.py

--- manage.py
+++ manage.py
@@ -4,24 +4,6 @@ import os
 import sys
 
 
-# os.environ.setdefault("POSTGRES_DB", "postgres")
-# os.environ.setdefault("POSTGRES_USER", "postgres")
-# os.environ.setdefault("POSTGRES_PASSWORD", "postgres")
-# os.environ.setdefault("POSTGRES_HOST", "localhost")
-# os.environ.setdefault("POSTGRES_PORT", "5432")
-#
-# os.environ.setdefault("BACKEND_HOST", "127.0.0.1")
-# os.environ.setdefault("BACKEND_PORT", "8002")
-#
-# os.environ.setdefault("FRONTEND_HOST", "127.0.0.1")
-# os.environ.setdefault("FRONTEND_PORT", "80")
-#
-# os.environ.setdefault("REDIS_HOST", "localhost")
-# os.environ.setdefault("REDIS_PORT", "6379")
-#
-# os.environ.setdefault("DJANGO_SECRET_KEY", "django-insecure-va=jrmk350#&^7a$gan2&v5#(m8r8$5gp(0dx52g7%8h4cb51p")
-# os.environ.setdefault("DEBUG_BACKEND", "true")
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
