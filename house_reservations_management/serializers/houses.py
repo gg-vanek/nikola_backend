9d77dee961ea6a0c7f4af9a510938fba0cd91a06
lvanekl <83428939+lvanekl@users.noreply.github.com>
2024-06-01 20:09:50 +0300

    fix

--- house_reservations_management/serializers/houses.py
+++ house_reservations_management/serializers/houses.py
@@ -23,34 +23,22 @@ class HouseListWithTotalPriceSerializer(HouseDetailSerializer):
         model = House
         fields = HouseDetailSerializer.Meta.fields + ["total_price"]
 
-    def validate_query_params(self) -> tuple[Datetime, Datetime, int] | None:
+    def get_total_price(self, house: House) -> int | None:
         query_params = self.context["request"].query_params
         try:
             check_in_date = Datetime.strptime(query_params.get("check_in_date"), "%d-%m-%Y").date()
             check_out_date = Datetime.strptime(query_params.get("check_out_date"), "%d-%m-%Y").date()
         except (ValueError, TypeError):
             # если нет какой-то из дат - мы не можем высчитать суммарную цену бронирования
-            logger.error("Invalid date format provided in query parameters")
             return None
 
         try:
-            total_persons_amount = query_params.get("total_persons_amount", self.instance.base_persons_amount)
+            total_persons_amount = query_params.get("total_persons_amount", house.base_persons_amount)
             total_persons_amount = int(total_persons_amount)
-            total_persons_amount = max(total_persons_amount, self.instance.base_persons_amount)
+            total_persons_amount = max(total_persons_amount, house.base_persons_amount)
         except (ValueError, TypeError):
             # если total_persons_amount не конвертируется в int -> не можем посчитать суммарную стоимость
-            logger.error("Invalid total_persons_amount provided in query parameters")
             return None
-
-        return check_in_date, check_out_date, total_persons_amount
-
-    def get_total_price(self, house: House) -> int | None:
-        validated_params = self.validate_query_params()
-        if not validated_params:
-            # если параметры не прошли валидацию, возвращаем None
-            return None
-
-        check_in_date, check_out_date, total_persons_amount = validated_params
 
         total_price = light_calculate_reservation_price(
             house=house,
